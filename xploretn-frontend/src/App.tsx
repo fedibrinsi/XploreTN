641045a853f9e90580a3f016bc7124c3fe31a7f8
Mhenni12 <medazizmhenni12@gmail.com>
2026-04-08 22:42:27 +0100

    feat: update routes

--- xploretn-frontend/src/App.tsx
+++ xploretn-frontend/src/App.tsx
@@ -4,6 +4,10 @@ import HomePage from './pages/HomePage';
 import ExploreActivities from './pages/ExploreActivities';
 import ExperienceDetails from './pages/ExperienceDetails';
 import BookingPage from './pages/BookingPage';
+import HostExperience from './pages/HostExperience';
+import UserProfile from './pages/UserProfile';
+import CuratorDashboard from './pages/CuratorDashboard';
+import MessagingInterface from './pages/MessagingInterface';
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
           <Route path="explore" element={<ExploreActivities />} />
           <Route path="experience/:id" element={<ExperienceDetails />} />
           <Route path="booking/:id" element={<BookingPage />} />
+          <Route path="host" element={<HostExperience />} />
+          <Route path="profile" element={<UserProfile />} />
+          <Route path="dashboard" element={<CuratorDashboard />} />
+          <Route path="messages" element={<MessagingInterface />} />
         </Route>
       </Routes>
     </BrowserRouter>
