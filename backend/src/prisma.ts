0bf9e73ab3053665aecf12f05070efafa4c9764e
Mhenni12 <medazizmhenni12@gmail.com>
2026-04-15 11:01:37 +0100

    fix: improve type safety: eliminate build errors due to broad type 'any'

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -3,10 +3,13 @@ dotenv.config(); // Doit être avant tout le reste
 
 // Ensure that the generated Prisma client is compatible with ESNext imports
 import prismaPkg from "../generated/prisma/index.js";
+import type { PrismaClient as PrismaClientType } from "../generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const { PrismaClient } = prismaPkg as any;
+// Improving type safety in Prisma
+// because "strict" is set to "true" in tsconfig.json )(for better type checking).
+const PrismaClientCtor = (prismaPkg as unknown as { PrismaClient: new (opts?: any) => PrismaClientType }).PrismaClient;
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClientCtor({ adapter });
 
 export default prisma;
